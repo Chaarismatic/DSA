2a6bbb18016402a02e27a8bf4a4fd7501ddffc59
Vaibhav Joshi <lynxofficial88@gmail.com>
2026-07-01 07:21:05 +0530

    L5 Update

--- Recursion/L5/TotalSubset.cpp
+++ Recursion/L5/TotalSubset.cpp
@@ -1,7 +1,32 @@
+// class Solution
+// {
+// public:
+//   void SUBSET(vector<int> &nums, vector<int> temp, vector<vector<int>> &ans, int index)
+//   {
+//     if (index == nums.size())
+//     {
+//       ans.push_back(temp);
+//       return;
+//     }
+//     SUBSET(nums, temp, ans, index + 1);
+//     temp.push_back(nums[index]);
+//     SUBSET(nums, temp, ans, index + 1);
+//   }
+
+//   vector<vector<int>> subsets(vector<int> &nums)
+//   {
+//     // Your code here
+//     vector<vector<int>> ans;
+//     vector<int> temp;
+//     SUBSET(nums, temp, ans, 0);
+//     return ans;
+//   }
+// };
+
 class Solution
 {
 public:
-  void SUBSET(vector<int> &nums, vector<int> temp, vector<vector<int>> &ans, int index)
+  void SUBSET(vector<int> &nums, vector<int> &temp, vector<vector<int>> &ans, int index)
   {
     if (index == nums.size())
     {
@@ -11,6 +36,7 @@ public:
     SUBSET(nums, temp, ans, index + 1);
     temp.push_back(nums[index]);
     SUBSET(nums, temp, ans, index + 1);
+    temp.pop_back();
   }
 
   vector<vector<int>> subsets(vector<int> &nums)
